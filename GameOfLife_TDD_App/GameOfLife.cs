b93044a8c5ffecd00589208227e337e6393e2e21
daniel.riddersporre@icloud.com <daniel.riddersporre@icloud.com>
2024-08-29 22:02:05 +0200

    added implementation of the methods with green tests. Game is working as intended!

--- GameOfLife_TDD_App/GameOfLife.cs
+++ GameOfLife_TDD_App/GameOfLife.cs
@@ -7,32 +7,98 @@ namespace GameOfLife_TDD_App
 
         public void InitializeGrid(int rows, int columns)
         {
-            throw new System.NotImplementedException();
+            _grid = new bool[rows, columns];
         }
 
         public bool[,] GetGrid()
         {
-            throw new System.NotImplementedException();
+            return _grid;
         }
 
         public void SetCell(int row, int column, bool isAlive)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _grid[row, column] = isAlive;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
         }
 
         public bool GetCell(int row, int column)
         {
-            throw new System.NotImplementedException();
+            return _grid[row, column];
         }
 
         public void Update()
         {
-            throw new System.NotImplementedException();
+            var newGrid = (bool[,])_grid.Clone();
+
+            for (int i = 0; i < newGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < newGrid.GetLength(1); j++)
+                {
+                    var liveNeighbours = CountLiveNeighbours(i, j);
+                    if (_grid[i, j])
+                    {
+                        if (liveNeighbours < 2 || liveNeighbours > 3)
+                        {
+                            newGrid[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (liveNeighbours == 3)
+                        {
+                            newGrid[i, j] = true;
+                        }
+                    }
+                }
+            }
+
+            _grid = newGrid;
         }
 
         private int CountLiveNeighbours(int row, int column)
         {
-            throw new System.NotImplementedException();
+            var count = 0;
+
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = column - 1; j <= column + 1; j++)
+                {
+                    if (i < 0 || i >= _grid.GetLength(0) || j < 0 || j >= _grid.GetLength(1) || (i == row && j == column))
+                    {
+                        continue;
+                    }
+
+                    if (_grid[i, j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public void DisplayGrid()
+        {
+            Console.Clear();
+            Console.WriteLine("+" + new string('-', _grid.GetLength(1)) + "+"); // Top border
+            for (int i = 0; i < _grid.GetLength(0); i++)
+            {
+                Console.Write("|"); // Left border
+                for (int j = 0; j < _grid.GetLength(1); j++)
+                {
+                    Console.Write(_grid[i, j] ? "X" : ".");
+                }
+                Console.WriteLine("|"); // Right border
+            }
+            Console.WriteLine("+" + new string('-', _grid.GetLength(1)) + "+"); // Bottom border
+            Console.WriteLine(); // Extra line for spacing
         }
     }
 }
--- GameOfLife_TDD_App/Program.cs
+++ GameOfLife_TDD_App/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GameOfLife_TDD_App;
+
+// Settings
+int generations = 1000;
+int rows = 25;
+int columns = 50;
+int millisecondsTimeout = 100;
+
+
+// Set up the initial state of the game
+
+GameOfLife gameOfLife = new GameOfLife();
+gameOfLife.InitializeGrid(rows, columns);
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        int random = new Random().Next(0, 2);
+
+        switch (random)
+        {
+            case 0:
+                gameOfLife.SetCell(i, j, false);
+                break;
+            case 1:
+                gameOfLife.SetCell(i, j, true);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+// Run the game for x generations
+for (int i = 0; i < generations; i++)
+{
+    gameOfLife.DisplayGrid();
+    gameOfLife.Update();
+
+    // Pause for a moment
+    Thread.Sleep(millisecondsTimeout);
+    Console.Clear();
+    Console.WriteLine("\x1b[3J");
+}
